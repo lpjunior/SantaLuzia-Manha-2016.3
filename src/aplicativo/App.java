50d9876f6cdce8dd25eafc761722f21d6c876b11
luis.junior <luis.junior@36DE194329.senacrjedu.local>
2017-12-01 11:39:06 -0200

    Interface e exception

---
+++ src/aplicativo/App.java
@@ -0,0 +1,35 @@
+package aplicativo;
+
+import entidade.Cliente;
+import entidade.Cliente.FormaPagamento;
+import entidade.Cliente.Tipo;
+import entidade.Funcionario;
+import entidade.Funcionario.Cargo;
+import util.Criptografia;
+
+public class App {
+	public static void main(String[] args) {
+
+		Cliente c = new Cliente();
+		Funcionario f = new Funcionario();
+		
+		c.setNome("Fulano de Tal");
+		c.setEmail("fulano@detal.com.br");
+		c.setLogin("fulano");
+		c.setSenha(Criptografia.geraMD5("123"));
+		c.setTelefone("(21) 98752-1234");
+		c.setFormaPagamento(FormaPagamento.CARTAO);
+		c.setTipo(Tipo.PESSOA_FISICA);
+		
+		System.out.println(c.autentica("fulano", "123") ? "Login efetuado" : "Falha ao efetuar login");
+		
+		f.setNome("José da Silva");
+		f.setEmail("jsilva@gmail.com");
+		f.setLogin("jsilva");
+		f.setSenha("2222");
+		f.setSetor("TI");
+		f.setCargo(Cargo.ANALISTA);
+		
+		System.out.println(f.autentica("jsilva", "222") ? "Login efetuado" : "Falha ao efetuar login");
+	}
+}
